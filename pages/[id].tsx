c341ad34c1a8b48d06f0ece23778ce37e8a54807
Anh Minh <nguyencaoanhminh@amitgroup.vn>
2022-07-05 16:57:36 +0700

    update notfound

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -11,7 +11,11 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const [resMovieDetails, resMovieCredits] = await Promise.all([fetch(`${API_URL}/movie/${slug}?api_key=${API_KEY}&language=en-US`), fetch(`${API_URL}/movie/${slug}/credits?api_key=${API_KEY}&language=en-US`)]);
   const responseDetails = await resMovieDetails.json();
   const responseCredits = await resMovieCredits.json();
-  // console.log(responseMovie);
+  if ((responseDetails.status_code || responseCredits.status_code) == 34) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: {
       responseDetails,
@@ -28,44 +32,48 @@ type PropType = {
 const UserDetail = (data: PropType) => {
   const router = useRouter();
   const imagePath = "https://image.tmdb.org/t/p/original";
-  console.log(data.responseCredits);
-  return (
-    <>
-      <div className="am-movie_detail">
-        <Image src={imagePath + data.responseDetails.backdrop_path} layout="fill" className="am-movie_detail-background" alt={"backdrop_path" + data.responseDetails.backdrop_path} />
-        <Row wrap={false} className="am-movie_detail-content" gutter={26}>
-          <Col flex="280px">
-            <Image src={imagePath + data.responseDetails.poster_path} alt={"poster" + data.responseDetails.title} width="280" height="400" className="am-movie_detail-image" />
-          </Col>
-          <Col flex="auto">
-            <div className="am-movie_detail-back" onClick={() => router.back()}>
-              Back to Homepage
-            </div>
-            <div className="am-movie_detail-genres">
-              {data.responseDetails.genres.map((item: any, key: number) => (
-                <span key={key}>{item.name}</span>
-              ))}
-            </div>
-            <h1 className="am-movie_detail-title">{data.responseDetails.title}</h1>
-            <div className="am-movie_detail-date">{new Date(data.responseDetails.release_date).toDateString()}</div>
-            <span className="am-movie_detail-description">
-              <div>Overview</div>
-              {data.responseDetails.overview}
-            </span>
-          </Col>
-        </Row>
-        <Row className="am-movie_detail-content">
-          <Col span={24} className="am-cast">
-            {data.responseCredits.cast.map((item: any, key: number) => (
-              <div key={key} className="am-cast_card">
-                <Image src={imagePath + item.profile_path} alt={item.character} width="140" height="180" />
-                <p className="am-cast_card-name">{item.original_name}</p>
+  console.log(data.responseCredits.status_code);
+  if (data.responseCredits.status_code == 34) {
+    return <></>;
+  } else {
+    return (
+      <>
+        <div className="am-movie_detail">
+          <Image src={imagePath + data.responseDetails.backdrop_path} layout="fill" className="am-movie_detail-background" alt={"backdrop_path" + data.responseDetails.backdrop_path} />
+          <Row wrap={false} className="am-movie_detail-content" gutter={26}>
+            <Col flex="280px">
+              <Image src={imagePath + data.responseDetails.poster_path} alt={"poster" + data.responseDetails.title} width="280" height="400" className="am-movie_detail-image" />
+            </Col>
+            <Col flex="auto">
+              <div className="am-movie_detail-back" onClick={() => router.back()}>
+                Back to Homepage
               </div>
-            ))}
-          </Col>
-        </Row>
-      </div>
-    </>
-  );
+              <div className="am-movie_detail-genres">
+                {data.responseDetails.genres.map((item: any, key: number) => (
+                  <span key={key}>{item.name}</span>
+                ))}
+              </div>
+              <h1 className="am-movie_detail-title">{data.responseDetails.title}</h1>
+              <div className="am-movie_detail-date">{new Date(data.responseDetails.release_date).toDateString()}</div>
+              <span className="am-movie_detail-description">
+                <div>Overview</div>
+                {data.responseDetails.overview}
+              </span>
+            </Col>
+          </Row>
+          <Row className="am-movie_detail-content">
+            <Col span={24} className="am-cast">
+              {data.responseCredits.cast.map((item: any, key: number) => (
+                <div key={key} className="am-cast_card">
+                  <Image src={imagePath + item.profile_path} alt={item.character} width="140" height="180" />
+                  <p className="am-cast_card-name">{item.original_name}</p>
+                </div>
+              ))}
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
 };
 export default UserDetail;
